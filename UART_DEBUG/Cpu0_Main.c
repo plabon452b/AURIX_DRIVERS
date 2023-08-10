20e208c25fa0e05b67b81b3f4acd757b7ccf1cbb
PlabonCSEDU24 <sakibhasan.24csedu.062@gmail.com>
2023-08-10 23:56:54 +0600

    UART DEBUG

---
+++ UART_DEBUG/Cpu0_Main.c
@@ -0,0 +1,32 @@
+#include "Ifx_Types.h"
+#include "IfxCpu.h"
+#include "IfxScuWdt.h"
+
+#include "UART.h"
+
+IFX_ALIGN(4) IfxCpu_syncEvent g_cpuSyncEvent = 0;
+
+void core0_main(void)
+{
+    IfxCpu_enableInterrupts();
+    
+    /* !!WATCHDOG0 AND SAFETY WATCHDOG ARE DISABLED HERE!!
+     * Enable the watchdogs and service them periodically if it is required
+     */
+    IfxScuWdt_disableCpuWatchdog(IfxScuWdt_getCpuWatchdogPassword());
+    IfxScuWdt_disableSafetyWatchdog(IfxScuWdt_getSafetyWatchdogPassword());
+    
+    /* Wait for CPU sync event */
+    IfxCpu_emitEvent(&g_cpuSyncEvent);
+    IfxCpu_waitEvent(&g_cpuSyncEvent, 1);
+    
+    /* Initialize the Shell Interface and the UART communication */
+    initSerialInterface();
+
+    println("hello debug %d %d",1,2);
+
+    while(1)
+    {
+       
+    }
+}
